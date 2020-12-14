f1b0735d6212f4580e3bd70e39ca05b00aaf88bc
HM9 <harry.e30s@gmail.com>
2020-12-13 21:38:37 -0500

    organizado link de conexión bbdd

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-//const db_link = "mongodb://mongo:27017/crudmobileusersdb";
-const db_link = "mongodb://127.0.0.1:27017/mongo";
+const db_link = "mongodb://mongo:27017/crudmobileusersdb";
+//const db_link = "mongodb://127.0.0.1:27017/mongo";
 
 const options = {
     useNewUrlParser: true,
